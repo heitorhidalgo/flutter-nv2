c12e85927510cf42086b942ddddf9384fc5a1e7f
Heitor Hidalgo <heitorhidalgo10@hotmail.com>
2026-05-24 18:10:15 -0300

    refactor: tipagem dos argumentos na rota de detalhes e ajustes no app_pages

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../routes/arguments/detalhes_card_args.dart';
 import '../views/catalogo_page.dart';
 import '../views/detalhes_card_page.dart';
 import '../views/home_page.dart';
@@ -7,25 +8,29 @@ import '../views/splash_page.dart';
 import 'app_routes.dart';
 
 class AppPages {
-  static Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-    AppRoutes.splash: (_) => const SplashPage(),
-    AppRoutes.login: (_) => const LoginPage(),
-    AppRoutes.home: (_) => const HomePage(),
-    AppRoutes.catalogo: (_) => const CatalogoPage(),
+  static final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+    AppRoutes.splash: (BuildContext context) => const SplashPage(),
+    AppRoutes.login: (BuildContext context) => const LoginPage(),
+    AppRoutes.home: (BuildContext context) => const HomePage(),
+    AppRoutes.catalogo: (BuildContext context) => const CatalogoPage(),
   };
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.detalhesCard:
-        final Map<String, dynamic> args =
-        settings.arguments as Map<String, dynamic>;
+        final DetalhesCardArguments args = settings.arguments as DetalhesCardArguments;
+
         return MaterialPageRoute(
-          builder: (_) => DetalhesCardPage(
-            carta: args['carta'],
-            modoRemover: args['modoRemover'],
-          ),
+          builder: (BuildContext context) {
+            return DetalhesCardPage(
+              carta: args.carta,
+              modoRemover: args.modoRemover,
+            );
+          },
         );
-      default: return null;
+
+      default:
+        return null;
     }
   }
 }
